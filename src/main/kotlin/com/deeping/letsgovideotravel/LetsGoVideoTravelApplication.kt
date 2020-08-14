119ee4591622248b9d6fbda147f4862f1375714e
jkjan <jkjan@naver.com>
2020-08-14 10:48:35 +0900

    initial commit

---
+++ src/main/kotlin/com/deeping/letsgovideotravel/LetsGoVideoTravelApplication.kt
@@ -0,0 +1,11 @@
+package com.deeping.letsgovideotravel
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LetsGoVideoTravelApplication
+
+fun main(args: Array<String>) {
+    runApplication<LetsGoVideoTravelApplication>(*args)
+}
---
+++ src/test/kotlin/com/deeping/letsgovideotravel/LetsGoVideoTravelApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.deeping.letsgovideotravel
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LetsGoVideoTravelApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
